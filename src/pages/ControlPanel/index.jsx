fa56b67990b0893ee26d944aef3affa3f026b251
Seba Ink <sebastian@gestioncervecera.com>
2020-11-23 17:44:30 -0300

    titles

--- src/pages/ControlPanel/index.jsx
+++ src/pages/ControlPanel/index.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./style.sass";
 import { Proyectos } from "../../components/Proyectos";
 import { UserInfo } from "../../components/UserInfo";
 import { Container, Grid } from "@material-ui/core";
+import { useTranslation } from "react-i18next";
 
 export const ControlPanel = () => {
+  const { t } = useTranslation();
+  useEffectt(() => {
+    document.title = t("proyectos-y-donaciones");
+  });
   return (
     <Container>
-      <Grid container className="container">
+      <Grid container className='container'>
         <Grid item xs={12} lg={3}>
           <UserInfo />
         </Grid>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Banner } from "../../components/Banner";
 import { Container, Grid } from "@material-ui/core";
 import "./style.sass";
 
 export const Home = () => {
+  useEffect(() => {
+    document.title = `Crowdfunding`;
+  });
   return (
     <Container className='home'>
       <Grid container className='top'>
